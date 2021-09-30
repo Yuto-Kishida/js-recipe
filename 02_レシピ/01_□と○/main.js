dead6fea916d555628dd1b31f67364c2c15a3e34
Yuto-Kishida <kisida0123@icloud.com>
2021-09-30 23:29:40 +0900

    [と〇]を学習しました。

---
+++ 02_レシピ/01_□と○/main.js
@@ -0,0 +1,15 @@
+//const figure = document.getElementById("figure")
+//const squareButton = document.getElementById("square-button")
+//const circleButton = document.getElementById("circle-button")
+//circleButton.onclick = function(){
+  //  figure.classList.add("rounded")
+//}
+
+//squareButton.onclick = function(){
+  //  figure.classList.remove("rounded")
+//}
+const figure = document.getElementById("figure")
+
+figure.onclick = function() {
+  figure.classList.toggle("rounded")
+}
--- 04＿ゼミ/week0/gitで保存する.md
+++ 04＿ゼミ/week0/gitで保存する.md
@@ -1,6 +1,6 @@
 ```bash
 git add .
-git commit -m "[3の倍数]を学習しました。"
+git commit -m "[カウンター]を学習しました。"
 git push origin master
 ```
 javaScriptの基礎完了
