4003d28aef4e3b1dd9e780dd56d6f54f2aefd69b
Yuto-Kishida <kisida0123@icloud.com>
2021-10-15 17:40:42 +0900

    [クイズ（1問）発展]を学習しました。

---
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -0,0 +1,9 @@
+const imageElement = document.getElementById("dog-image")
+
+fetch("https://api.thecatapi.com/v1/images/search")
+  .then((res) => {
+    return res.json() 
+  })
+  .then((data) => {
+    imageElement.src = data.message 
+  })
--- 04＿ゼミ/week0/gitで保存する.md
+++ 04＿ゼミ/week0/gitで保存する.md
@@ -1,6 +1,6 @@
 ```bash
 git add .
-git commit -m "[week2　恵比寿のおいしいカレー屋さん]を学習しました。"
+git commit -m "[クイズ（1問）発展★]を学習しました。"
 git push origin master
 ```
 javaScriptの基礎完了
