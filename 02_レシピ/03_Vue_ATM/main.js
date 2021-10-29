8b6acdbaa8c7b78025748b25b84d6a88e5b609bd
Yuto-Kishida <kisida0123@icloud.com>
2021-10-29 18:50:29 +0900

    [Vueでカウンターを作る]を学習しました。

---
+++ 02_レシピ/03_Vue_ATM/main.js
@@ -0,0 +1,33 @@
+new Vue({
+    el:"#app",
+    data: {
+        nyuryokugaku: "",
+        zandaka: 0,
+        log:[
+            {
+                date: new Date("2021-07-23T12:00:00"),
+                type: "入金",
+                money: 1000
+              },
+        ]
+    },
+    methods:{
+        nyukinn:function(){
+            this.zandaka += Number(this.nyuryokugaku);
+            const now = new Date();
+            this.log.push({date:now, type:"入金",money:Number(this.nyuryokugaku)});
+        },
+        syukkinn:function(){
+            this.zandaka -= Number(this.nyuryokugaku);
+            const now = new Date();
+            this.log.push({date:now, type:"出金",money:Number(this.nyuryokugaku)});
+        },
+    },
+    computed:{
+        syukkinn:function(){
+            if(this.zandaka < Number(this.nyuryokugaku)){
+                this.syukkinn.bind=true;
+            }
+        }
+    }
+});
--- 04＿ゼミ/week0/gitで保存する.md
+++ 04＿ゼミ/week0/gitで保存する.md
@@ -1,6 +1,6 @@
 ```bash
 git add .
-git commit -m "[配列でメモアプリをつくろう作ろうローカルストレージ]を学習しました。"
+git commit -m "[Vueワーク1,2]を学習しました。"
 git push origin master
 ```
 javaScriptの基礎完了
